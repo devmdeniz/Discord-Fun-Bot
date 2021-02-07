f04a7e4293712bcbe750fe731f9ba01178fe02bb
devmdeniz <mehmetdeniz1397@gmail.com>
2021-02-07 13:59:21 +0300

    Code Reboot command

---
+++ commands/reboot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+
+exports.run = (client, message, args) => {
+message.channel.sendMessage(' **Reboot?**')
+.then(() => {
+  message.channel.awaitMessages(response => response.content === "yes", {
+    max: 1,
+    time: 30000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+      message.channel.sendMessage('  **Rebooted** :ok_hand:   ').then(message => {
+      console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] :space_invader: **Restarting** :space_invader:`)
+      process.exit(0);
+    }).catch(console.error)
+    })
+    .catch(() => {
+      message.channel.sendMessage('ERROR');
+    });
+});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'reboot',
+  description: '[YAPIMCI]',
+  usage: 'reboot'
+};
